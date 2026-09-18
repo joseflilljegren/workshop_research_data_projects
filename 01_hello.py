14e3c37ed7f559fe73c54985f75f459beff63617
nJGL <login@lilljegren.com>
2026-09-18 11:39:55 +0200

    Examples for  on simple files

---
+++ 01_hello.py
@@ -0,0 +1,10 @@
+
+def main():
+    """Main."""
+    print('The workshop')
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ 01_hello_variant.py
@@ -0,0 +1,17 @@
+
+def main():
+    """Main."""
+    print('This is different')
+
+
+
+
+def useless_function(x):
+    """This function is useless."""
+    return x+x
+
+
+
+
+if __name__ == "__main__":
+    main()
