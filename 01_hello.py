61a1088d888ec6319b09533ffa10a090177ae379
nJGL <login@lilljegren.com>
2026-09-24 14:59:31 +0200

    A test commit

--- 01_hello.py
+++ 01_hello.py
@@ -1,9 +1,14 @@
-
+"""
+    Just a hello world
+"""
 def main():
     """Main."""
     print('The workshop')
 
 
+def power(x):
+    """Power function."""
+    return x * x
 
 
 if __name__ == "__main__":
